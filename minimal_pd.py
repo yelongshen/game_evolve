c30d754874fe56e40d216929f603fd2765174565
yelong shen <yeshe@microsoft.com>
2025-09-25 21:35:58 -0700

    length

--- minimal_pd.py
+++ minimal_pd.py
@@ -9,5 +9,5 @@ import time
 if __name__ == "__main__":
     start = time.time()
     verbose = logging.getLogger().getEffectiveLevel() <= logging.DEBUG
-    run_sim(steps=200000, N=50, history_len=256, p_death=0.002, log_every=100, out_csv="sim_log_new.csv", pairs_per_step=10, train_every=10, verbose=verbose)
+    run_sim(steps=200000, N=50, history_len=512, p_death=0.002, log_every=100, out_csv="sim_log_new.csv", pairs_per_step=10, train_every=10, verbose=verbose)
     print("done", time.time() - start)
